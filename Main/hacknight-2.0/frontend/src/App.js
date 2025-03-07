60e809affa1e18d12cef5fc93c1d511eb9b0c715
Akshit Bhatt <akshitbhattab@gmail.com>
2025-03-07 08:07:15 +0530

    Update App.js

--- Main/hacknight-2.0/frontend/src/App.js
+++ Main/hacknight-2.0/frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NeuroNav from './components/NeuroNav';
+import LoginPage from './components/LoginPage';
 import NeuroNavDashboard from './components/NeuroNavDashboard';
-
+import GameDashboard from './components/GameDashboard'; // Import the GameDashboard component
+import SetupPage from './components/SetupPage';
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<NeuroNav />} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/neuronavdashboard" element={<NeuroNavDashboard />} />
+          <Route path="/games" element={<GameDashboard />} /> {/* Add GameDashboard route */}
+          <Route path="/setpage" element={<SetupPage/>} />
         </Routes>
       </div>
     </Router>
